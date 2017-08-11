d4f44afc8447c7045df68edb730899015ba1a277
OOHASHI Daichi <dico.leque.comicron@gmail.com>
2017-08-11 15:40:51 +0900

    moving ops

--- quadkey.scm
+++ quadkey.scm
@@ -5,7 +5,12 @@
           valid-longitude? max-longitude min-longitude
           quadkey->string quadkey->latitude&longitude
           merge-bits unmerge-bits
-          string->quadkey)
+          string->quadkey
+          quadkey-has-parent? quadkey-parent
+          quadkey-has-child? quadkey-child
+          quadkey-sibling
+          quadkey-left-neighbor quadkey-right-neighbor
+          quadkey-top-neighbor quadkey-bottom-neighbor)
   (use srfi-11)
   (use srfi-13)
   (use srfi-60)
@@ -132,3 +137,127 @@
                         (logbit? (+ 1 (* 2 n)) bits)))
          (n n (- n 1)))
         ((< n 0) (values x y)))))
+
+(define (quadkey-position qk)
+  (ecase (logand 3 (quadkey-value qk))
+    ((#b00) 'top-left)
+    ((#b01) 'top-right)
+    ((#b10) 'bottom-left)
+    ((#b11) 'bottom-right)))
+
+(define (quadkey-has-parent? qk)
+  (> (quadkey-level qk) min-quadkey-level))
+
+(define (quadkey-parent qk)
+  (assume (quadkey-has-parent? qk))
+  (%make-quadkey (%quadkey-parent (quadkey-value qk))
+                 (- (quadkey-level qk) 1)))
+
+(define (quadkey-has-child? qk)
+  (< (quadkey-level qk) max-quadkey-level))
+
+(define (quadkey-child qk pos)
+  (assume (quadkey-has-child? qk))
+  (%make-quadkey (%quadkey-child (quadkey-value qk) pos)
+                 (+ (quadkey-level qk) 1)))
+
+(define (quadkey-sibling qk pos)
+  (%make-quadkey (%quadkey-child (%quadkey-parent (quadkey-value qk))
+                                 pos)
+                 (quadkey-level qk)))
+
+(define (%quadkey-position->integer pos)
+  (ecase pos
+    ((top-left) #b00)
+    ((top-right) #b01)
+    ((bottom-left) #b10)
+    ((bottom-right) #b11)))
+
+(define (%quadkey-parent v)
+  (arithmetic-shift v -2))
+
+(define (%quadkey-child v pos)
+  (logior (arithmetic-shift v 2)
+          (%quadkey-position->integer pos)))
+
+(define (%quadkey-neighbor f qk)
+  (let-values (((recur pos) (f qk)))
+    (if recur
+        (quadkey-child (%quadkey-neighbor f (quadkey-parent qk))
+                       pos)
+        (quadkey-sibling qk pos))))
+
+(define quadkey-left-neighbor
+  (let ()
+    (define (f qk)
+      (let ((has-parent (quadkey-has-parent? qk)))
+        (case (quadkey-position qk)
+          ((top-left)
+           (values has-parent 'top-right))
+          ((top-right)
+           (values #f 'top-left))
+          ((bottom-left)
+           (values has-parent 'bottom-right))
+          ((bottom-right)
+           (values #f 'bottom-left)))))
+    (lambda (qk)
+      (%quadkey-neighbor f qk))))
+
+(define quadkey-right-neighbor
+  (let ()
+    (define (f qk)
+      (let ((has-parent (quadkey-has-parent? qk)))
+        (case (quadkey-position qk)
+          ((top-left)
+           (values #f 'top-right))
+          ((top-right)
+           (values has-parent 'top-left))
+          ((bottom-left)
+           (values #f 'bottom-right))
+          ((bottom-right)
+           (values has-parent 'bottom-left)))))
+    (lambda (qk)
+      (%quadkey-neighbor f qk))))
+
+(define (invalid-op msg)
+  (error msg))
+
+(define quadkey-top-neighbor
+  (let ()
+    (define (f qk)
+      (let ((has-parent (quadkey-has-parent? qk)))
+        (case (quadkey-position qk)
+          ((top-left)
+           (unless has-parent
+             (invalid-op "no neighbor"))
+           (values has-parent 'bottom-left))
+          ((top-right)
+           (unless has-parent
+             (invalid-op "no neighbor"))
+           (values has-parent 'bottom-right))
+          ((bottom-left)
+           (values #f 'top-left))
+          ((bottom-right)
+           (values #f 'top-right)))))
+    (lambda (qk)
+      (%quadkey-neighbor f qk))))
+
+(define quadkey-bottom-neighbor
+  (let ()
+    (define (f qk)
+      (let ((has-parent (quadkey-has-parent? qk)))
+        (case (quadkey-position qk)
+          ((top-left)
+           (values #f 'bottom-left))
+          ((top-right)
+           (values #f 'bottom-right))
+          ((bottom-left)
+           (unless has-parent
+             (invalid-op "no neighbor"))
+           (values has-parent 'top-left))
+          ((bottom-right)
+           (unless has-parent
+             (invalid-op "no neighbor"))
+           (values has-parent 'top-right)))))
+    (lambda (qk)
+      (%quadkey-neighbor f qk))))
--- test-quadkey.scm
+++ test-quadkey.scm
@@ -12,6 +12,68 @@
 
 (define test-count 10000)
 
+(define level-1-map
+  '#(
+     #("0" "1")
+     #("2" "3")
+     ))
+
+(define level-2-map
+  '#(
+     #("00" "01" "10" "11")
+     #("02" "03" "12" "13")
+     #("20" "21" "30" "31")
+     #("22" "23" "32" "33")
+     ))
+
+(define level-3-map
+  '#(
+     #("000" "001" "010" "011" "100" "101" "110" "111")
+     #("002" "003" "012" "013" "102" "103" "112" "113")
+     #("020" "021" "030" "031" "120" "121" "130" "131")
+     #("022" "023" "032" "033" "122" "123" "132" "133")
+     #("200" "201" "210" "211" "300" "301" "310" "311")
+     #("202" "203" "212" "213" "302" "303" "312" "313")
+     #("220" "221" "230" "231" "320" "321" "330" "331")
+     #("222" "223" "232" "233" "322" "323" "332" "333")
+     ))
+
+(define (test-neighbors qmap)
+  (define (test-neighbor msg orig expected f)
+    (test* msg
+           expected
+           (quadkey->string (f (string->quadkey orig)))))
+  (let ((size (vector-length qmap)))
+    (dotimes (i size)
+      (dotimes (j size)
+        (let ((orig (~ qmap j i)))
+          (test-neighbor (format "quadkey-left-neighbor of ~A" orig)
+                         orig
+                         (~ qmap j (modulo (- i 1) size))
+                         quadkey-left-neighbor)
+          (test-neighbor (format "quadkey-right-neighbor of ~A" orig)
+                         orig
+                         (~ qmap j (modulo (+ i 1) size))
+                         quadkey-right-neighbor)
+          (test-neighbor (format "quadkey-top-neighbor of ~A" orig)
+                         orig
+                         (if (= j 0)
+                             (test-error)
+                             (~ qmap (modulo (- j 1) size) i))
+                         quadkey-top-neighbor)
+          (test-neighbor (format "quadkey-bottom-neighbor of ~A" orig)
+                         orig
+                         (if (= j (- size 1))
+                             (test-error)
+                             (~ qmap (modulo (+ j 1) size) i))
+                         quadkey-bottom-neighbor)
+          )))))
+
+(test-neighbors level-1-map)
+(test-neighbors level-2-map)
+(test-neighbors level-3-map)
+
+#;
 (let ((n test-count))
   (do-generator (level&lat&lng
                  (gtake (tuples-of
